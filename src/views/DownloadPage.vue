08177ab76165ef8780bfafab6442dc8bce31f041
Potato <985762543@qq.com>
2017-06-11 09:57:54 +0800

    add download page

--- src/views/DownloadPage.vue
+++ src/views/DownloadPage.vue
@@ -1,9 +1,8 @@
 <template>
   <Page id="download-page">
-
+    <img src="../assets/qrcode.png" />
+    <div>扫描识别二维码，前往AppStore下载应用</div>
+    <div>如果按钮失效，请点击右上角菜单选择用safari浏览器打开</div>
+    <div class="button" @click="$router.back()">返回D4ME WEBAPP</div>
   </Page>
 </template>
-
-<script>
-    export default {}
-</script>
