ea3106001c6b53666835e6714045f94e2ee94c91
Potato <985762543@qq.com>
2017-06-21 20:35:05 +0800

    add login jump state

--- src/utils/wx_login.js
+++ src/utils/wx_login.js
@@ -1,3 +1,7 @@
 export default function(){
-  window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5b486ab603b7f0a7&redirect_uri=http://app.d4me.com/api/v1/users/wx_login_code&response_type=code&scope=snsapi_userinfo#wechat_redirect`
+  var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5b486ab603b7f0a7&redirect_uri=http://app.d4me.com/api/v1/users/wx_login_code&response_type=code&scope=snsapi_userinfo#wechat_redirect'
+  if(/^\/products\/(\d+)/.test(window.location.pathname)){
+    url+=`state=product-${RegExp.$1}`
+  }
+  window.location.href = url
 }
