c9eb250303d889821e3d8c4babdfaf17946d1c82
Potato <985762543@qq.com>
2017-06-29 21:35:47 +0800

    fix url

--- src/utils/wx_login.js
+++ src/utils/wx_login.js
@@ -1,7 +1,8 @@
 export default function(){
-  var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5b486ab603b7f0a7&redirect_uri=http://app.d4me.com/api/v1/users/wx_login_code&response_type=code&scope=snsapi_userinfo#wechat_redirect'
+  var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5b486ab603b7f0a7&redirect_uri=http://app.d4me.com/api/v1/users/wx_login_code&response_type=code&scope=snsapi_userinfo'
   if(/^\/products\/(\d+)/.test(window.location.pathname)){
-    url+=`state=product-${RegExp.$1}`
+    url+=`&state=product-${RegExp.$1}`
   }
+  url+='#wechat_redirect'
   window.location.href = url
 }
